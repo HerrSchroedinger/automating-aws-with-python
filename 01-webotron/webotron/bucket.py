6dd0fa91c1d6afa28634bb3e3119edb5b4ba5dcd
HerrSchroedinger <tolindner82@icloud.com>
2019-10-20 11:37:44 +0200

    output website when syncing

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -3,16 +3,28 @@
 """Classes for S3 Buckets."""
 
 import mimetypes
+import util
 from pathlib import Path
 from botocore.exceptions import ClientError
 
 
 class BucketManager:
     """Manage an S3 Bucket."""
+
     def __init__(self, session):
         """Create a BucketManager Object."""
         self.session = session
         self.s3 = self.session.resource('s3')
+
+    def get_region_name(self, bucket):
+        """Get the bucket's region name."""
+        bucket_location = self.s3.meta.client.get_bucket_location(Bucket=bucket.name)
+
+        return bucket_location["LocationConstraint"] or 'us-east-1'
+
+    def get_bucket_url(self, bucket):
+        """Get the website URL for this bucket"""
+        return "http://{}.{}".format(bucket.name, util.get_endpoint(self.get_region_name(bucket)).host)
 
     def all_buckets(self):
         """Get an iterator for all buckets"""
@@ -28,7 +40,8 @@ class BucketManager:
 
         try:
             s3_bucket = self.s3.create_bucket(Bucket=bucket_name,
-                                         CreateBucketConfiguration={'LocationConstraint': self.session.region_name})
+                                              CreateBucketConfiguration={
+                                                  'LocationConstraint': self.session.region_name})
         except ClientError as error:
             if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
                 s3_bucket = self.s3.Bucket(bucket_name)
