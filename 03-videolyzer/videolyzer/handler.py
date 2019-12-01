59603b22587fca7451f8c907e2ab0514a51a3b7f
HerrSchroedinger <tolindner82@icloud.com>
2019-12-01 18:51:20 +0100

    Trigger rekognition job

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,2 +1,23 @@
-def startProcessingVideo(event, context):
-    print(event)
+import urllib
+import boto3
+
+
+def start_label_detection(bucket, key):
+    rekognition_client = boto3.client('rekognition')
+    response = rekognition_client.start_label_detection(
+        Video={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key
+            }
+        })
+
+    print(response)
+
+
+def start_processing_video(event, context):
+    for record in event['Records']:
+        start_label_detection(
+            record['s3']['bucket']['name'],
+            urllib.parse.unquote_plus(record['s3']['object']['key'])
+        )
